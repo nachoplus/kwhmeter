f9ee57c1061ab7d218053eb9d4a06c54d5293012
nachoplus <mas.ignacio@gmail.com>
2024-07-24 01:01:09 +0200

    update setup.* and pipupload.sh to use twine for upload to pypi

--- pipupload.sh
+++ pipupload.sh
@@ -3,6 +3,7 @@ git tag --delete ${tagname}
 git tag ${tagname}   -m "first pip version"
 git push origin :refs/tags/${tagname}
 git push --tags origin main
+rm dist/*
 python3 -m build
 twine upload -r pypi dist/*
 
